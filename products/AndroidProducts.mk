14eb9637ab9ac0cdbdffa11499df10f6bca7b9f3
doktaphex <jozef.kruszynski@gmail.com>
2014-11-24 21:00:50 +0000

    added make file

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/frankened_flo.mk \
     $(LOCAL_DIR)/frankened_hammerhead.mk \
-    $(LOCAL_DIR)/frankened_mako.mk
+    $(LOCAL_DIR)/frankened_mako.mk \
+    $(LOCAL_DIR)/frankened_grouper.mk
