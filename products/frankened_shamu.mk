e88fad2d64f1970be1c20970d3bc6973c5256f30
sykopompos <sykopompos@gmail.com>
2014-12-02 23:16:42 -0500

    clean up shamu, move the modified fstab here so I don't need to track the whole moto device

--- products/frankened_shamu.mk
+++ products/frankened_shamu.mk
@@ -11,8 +11,12 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Shamu
 PRODUCT_MANUFACTURER := motorola
 
-$(call inherit-product, device/moto/shamu/device.mk)
-$(call inherit-product-if-exists, vendor/moto/shamu/device-vendor.mk)
-
 # Build prop fingerprint overrides
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="shamu" BUILD_FINGERPRINT="google/shamu/shamu:5.0/LRX21Q/1570415:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 5.0 LRX21T 1570415 release-keys"
+
+# Local path for prebuilts
+LOCAL_PATH:= vendor/frank/prebuilts/shamu
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/root/fstab.shamu:root/fstab.shamu 
+   
