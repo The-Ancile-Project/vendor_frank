b3ccde0215377ee8ef96e10ce51da338f8d25e73
sykopompos <sykopompos@gmail.com>
2015-03-13 23:41:58 -0400

    update to PI!!!!!!

--- configs/common.mk
+++ configs/common.mk
@@ -26,7 +26,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.amr.wideband=1 \
     net.tethering.noprovisioning=true \
     persist.sys.dun.override=0 \
-    ro.mod.display.version=13-release-the-kraken \
+    ro.mod.display.version=14-PI \
     drm.service.enabled=true \
      
 # Common overlays
